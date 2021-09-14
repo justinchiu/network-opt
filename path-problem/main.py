2b49b1ea7d4a117f99d11ae99be34af1e61fe79e
Justin Chiu <justinchiu@g.harvard.edu>
2021-09-14 02:41:43 -0400

    debugging

--- path-problem/main.py
+++ path-problem/main.py
@@ -13,11 +13,14 @@ from typing import NamedTuple, List
 class Problem(NamedTuple):
     graph: nx.Graph
     paths: List[List[int]]
-    demand: np.ndarray
-    constraints: np.ndarray
-    r2p: np.ndarray
-    r2p_tup: np.ndarray
-    e2p: np.ndarray
+    demand: np.ndarray # in adjacency matrix form
+    constraints: np.ndarray # in adjacency matrix form
+    #r2p: np.ndarray
+    #r2p_tup: np.ndarray
+    P: int
+    Pr: int
+    e2p: np.ndarray # binary map from edge to which paths appear
+    p2e: np.ndarray # binary map from path to included edges
 
 
 basedir = Path("teavar/code/data/B4")
@@ -26,11 +29,14 @@ nodes_file = basedir / "nodes.txt"
 topology_file = basedir / "topology.txt"
 path_file = basedir / "paths/FFC"
 
+scale = 1000
+#scale = 1
+
 def process_demands(demand_file):
     with demand_file.open() as f:
         demands = [[float(d) for d in line.split()] for line in f]
     demands = np.array(demands).reshape((-1, 12, 12))
-    return demands
+    return demands[0] / scale
 
 def process_topology(topology_file):
     with topology_file.open() as f:
@@ -52,7 +58,7 @@ def process_topology(topology_file):
         adjacency_constraints = np.zeros((V,V))
         adjacency_constraints[edges[:,0], edges[:,1]] = constraints
 
-        return adjacency_constraints
+        return adjacency_constraints / scale
 
 def process_paths(path_file, demand):
     with path_file.open() as f:
@@ -76,7 +82,6 @@ def process_paths(path_file, demand):
             else:
                 raise ValueError
 
-        P = r2p.sum()
         # sparse version
         # r2p_tuple[s,t] = [start, end)
         r2p_tuple = np.zeros((V*V, 2), dtype=int)
@@ -84,14 +89,18 @@ def process_paths(path_file, demand):
         r2p_tuple[1:-1,1] = r2p_tuple[2:,0]
         r2p_tuple[-1,1] = P
 
-        # dense version, assuming each request has the same number of paths
-        Pk = r2p.max()
+        Pr = r2p.max()
+        #P = r2p.sum()
+        P = V * V * Pr
 
         # reset file
         f.seek(0)
 
+        requests = []
+
         # binary tensor: whether a path passes through an edge
         e2p = np.zeros((V, V, P))
+        p2e = np.zeros((P, V, V))
         p = 0
         for line in f:
             tokens = line.split()
@@ -102,10 +111,17 @@ def process_paths(path_file, demand):
                 # start of edge
                 r_src = int(tokens[0][1:]) - 1
                 r_tgt = int(tokens[2][1:]) - 1
+
+                if r_src == r_tgt - 1:
+                    # add Pr null paths for diagonal entries
+                    # WARNING: assumes that every src-tgt pair / request has paths
+                    p += Pr
+                requests.append([])
             elif tokens[-2] == "@":
                 # path internal
                 assert(len(tokens) >= 5)
 
+                edges = []
                 for edge in tokens[1:-3]:
                     # middle edges
                     src, tgt, _  = edge.split(",")
@@ -113,24 +129,30 @@ def process_paths(path_file, demand):
                     e_tgt = int(tgt[1:-1]) - 1
 
                     e2p[e_src, e_tgt, p] = 1
+                    edges.append((e_src, e_tgt))
+
+                    p2e[p, e_src, e_tgt] = 1
+                requests[-1].append(edges)
                 p += 1
             else:
                 raise ValueError
-        return r2p, r2p_tuple, e2p
+        return requests, r2p, r2p_tuple, P, Pr, e2p, p2e
 
 def load_problem():
     demands = process_demands(demand_file)
     constraints = process_topology(topology_file)
-    r2p, e2p = process_paths(path_file, demands[0])
-    graph = nx.Graph(adjacency)
+    paths, r2p, r2p_tup, P, Pr, e2p, p2e = process_paths(path_file, demands)
+    graph = nx.Graph(constraints)
 
     return Problem(
         graph,
         paths,
-        demands[0],
+        demands,
         constraints,
-        r2p,
+        P,
+        Pr,
         e2p,
+        p2e,
     )
 
 
@@ -147,51 +169,58 @@ nx.draw(G, pos, with_labels=True)
 plt.savefig('example_graph.png')
 #plt.show()
 
-K = len(problem.paths)
 V = problem.constraints.shape[0]
 
-pi_r = np.zeros(())
-
-pi_e = np.zeros((V, V, K))
-for p, path in enumerate(problem.paths):
-    for s,t in zip(path, path[1:]):
-        pi_e[s,t,p] = 1
+# construct dense_r2p as a sequence of [ 0 ... 1 ... 0 ] stacked vectors
+dense_r2p = []
+for v in range(V * V):
+    vec = np.zeros(problem.P)
+    vec[v*problem.Pr:(1+v)*problem.Pr] = 1
+    dense_r2p.append(vec)
+dense_r2p = np.vstack(dense_r2p)
 
 # CVXPY
-X = cp.Variable(K)
+X = cp.Variable(problem.P, nonneg=True)
 objective = cp.Minimize(-cp.sum(X))
 constraints = [
-    pi_e.reshape(V*V, K) @ X <= problem.constraints.reshape(V*V),
-    X >= 0,
+    dense_r2p @ X <= problem.demand.reshape((V*V,)),
+    problem.e2p.reshape((V*V, problem.P)) @ X <= problem.constraints.reshape((V*V,)),
 ]
 
 prob = cp.Problem(objective, constraints)
-result = prob.solve()
-print(X.value)
-print(constraints[0].dual_value)
+result = prob.solve(verbose=True)
+#print(X.value)
+#print(constraints[0].dual_value)
+print(prob.value)
 
+sol = X.value
 
+print("Constraint residuals")
+print((dense_r2p @ sol - problem.demand.reshape((V*V,))).max())
+print((problem.e2p.reshape((V*V, problem.P)) @ sol - problem.constraints.reshape((V*V,))).max())
 
 # ADMM
 
 def naive_admm_solver(
     problem,
-    pi_e,
     rho,
-    x, z, s,
-    lambda1, lambda2,
+    x, z, s1, s3,
+    lambda1, lambda3, lambda4,
     num_iters = 100,
 ):
+    Pr = problem.Pr
+
+    d = problem.demand.reshape(-1)
     c = problem.constraints.reshape(-1)
 
-    path_lens = np.array([len(p) for p in problem.paths])
+    e2p = problem.e2p.reshape(V*V, -1)
 
-    L = sum(path_lens)
+    e2p_flat = e2p.astype(int)
+    num_paths_for_edge = e2p_flat.sum(-1)
 
-    pi_e_flat = pi_e.reshape(V*V, K).astype(int)
-    num_paths_for_edge = pi_e_flat.sum(-1)
+    e2p_flat_bool = e2p.reshape(-1).astype(bool)
 
-    pi_e_flat_bool = pi_e.reshape(-1).astype(bool)
+    A_r_inv = np.linalg.inv(np.ones((Pr, Pr)) + np.eye(Pr))
 
     A_invs = [
         np.linalg.inv(np.ones((k, k)) + np.eye(k))
@@ -206,59 +235,61 @@ def naive_admm_solver(
 
     for iter in range(num_iters):
         # x update
-        #x_numerator = 1 - (lambda2 * pi_e_flat).sum(0) + rho * (pi_e_flat * z).sum(0)
-        x_numerator = (
-            1 + lambda1 - (lambda4 * pi_e_flat).sum(0)
-            + rho * ()
-            + rho * (pi_e_flat * z).sum(0)
+        b = (
+            (-1 - lambda1[:,None] + (e2p * lambda4).sum(0).reshape(V*V, -1))
+            + rho * (-d + s1 + (e2p * z).sum(1))[:,None]
         )
-        #x_denominator = (path_lens-1) * rho
-        x_denominator = path_lens * rho
-        x = np.maximum(0, x_numerator / x_denominator)
+        x = -np.einsum("ab,nb->na", A_r_inv / rho, b).reshape(-1)
+        x = np.maximum(0, x)
 
         # z update
         for e in range(c.size):
             if A_invs[e] is not None:
                 A_inv = A_invs[e] / rho
-                path_ix = pi_e_flat[e].astype(bool)
+                path_ix = e2p[e].astype(bool)
                 #b = (-lambda1[e,None] - lambda2[e, path_ix] + lambda5[e, path_ix]
-                b = (-lambda1[e,None] - lambda2[e, path_ix]
-                    + rho * (-c[e,None] + s[e,None] - x[path_ix]))
+                b = (-lambda3[e,None] - lambda4[e, path_ix]
+                    + rho * (-c[e,None] + s3[e,None] - x[path_ix]))
                 z[e, path_ix] = -A_inv @ b
-        #z = np.maximum(0, z)
+        z = np.maximum(0, z)
         #import pdb; pdb.set_trace()
          
         # s update
-        s = np.maximum(0,
-            #(lambda1 - lambda4 + rho * (c - (pi_e_flat * z).sum(-1))) / (2 * rho))
-            (lambda1 + rho * (c - (pi_e_flat * z).sum(-1))) / (rho))
+        s1 = (lambda1 + rho * (d - x.reshape(V*V, -1).sum(1))) / rho
+        s3 = (lambda3 + rho * (c - (e2p * z).sum(-1))) / rho
+        s1 = np.maximum(0, s1)
+        s3 = np.maximum(0, s3)
 
-        r1 = c - (pi_e_flat * z).sum(-1) - s
-        r2 = x[None] - z
+        r1 = d - x.reshape(V*V, -1).sum(1) - s1
+        r3 = c - (e2p * z).sum(-1) - s3
+        r4 = x[None] - z
 
         # lambda update
         lambda1 += rho * r1
+        lambda3 += rho * r3
+        lambda4 += rho * r4
 
-        lambda2 = lambda2.reshape(-1)
-        lambda2[pi_e_flat_bool] += rho * r2.reshape(-1)[pi_e_flat_bool]
-        #np.add.at(lambda2, pi_e_flat, rho * r2[pi_e_flat])
-        lambda2 = lambda2.reshape(pi_e_flat.shape)
+        lambda4 = lambda4.reshape(-1)
+        lambda4[e2p_flat_bool] += rho * r4.reshape(-1)[e2p_flat_bool]
+        lambda4 = lambda4.reshape(e2p_flat.shape)
 
-    return x, z, s, lambda1, lambda2
+    return x, z, s1, s3, lambda1, lambda3, lambda4, r1, r3, r4
 
 
-s = np.zeros((V,V))
-#np.copyto(s, problem.constraints)
-s = s.reshape(-1)
-
-x, z, s, l1, l2 = naive_admm_solver(
+x, z, s1, s3, l1, l3, l4, r1, r3, r4 = naive_admm_solver(
     problem,
-    pi_e,
-    1,
-    np.zeros((K,)), np.zeros((V*V, K)), s,
-    np.zeros((V*V,)), np.zeros((V*V, K)),
+    rho = 1,
+    x = np.zeros((problem.P,)),
+    z = np.zeros((V*V, problem.P)),
+    s1 = np.zeros((V*V,)),
+    s3 = np.zeros((V*V,)),
+    lambda1 = np.zeros((V*V,)),
+    lambda3 = np.zeros((V*V,)),
+    lambda4 = np.zeros((V*V, problem.P)),
     num_iters = 1000,
 )
-print(s)
+#print(s1)
+#print(s3)
+print(-x.sum())
 
 import pdb; pdb.set_trace()
